8e0594352e87daafba7741bf4c8b8a74026343a4
Treee <jonathon.lindemann@gmail.com>
2020-06-15 17:44:22 -0600

    adding footer disclaimer

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,25 @@
         <router-view></router-view>
       </v-container>
     </v-main>
-    <v-footer app>Test footer</v-footer>
+    <v-footer app center>
+      <div class="my-footer">
+        <div>
+          Created by
+          <a href="https://www.twitch.tv/itsatreee">ItsATreee</a>
+          (2020) -
+          <a href="https://github.com/Treee/aoe-statistics-ui">GitHub</a>
+        </div>
+        <div>
+          Age of Empires II © Microsoft Corporation.
+          <b>AoE Statistics for Age of Empires II</b> was created under Microsoft's
+          "
+          <a
+            href="https://www.xbox.com/en-us/developers/rules"
+          >Game Content Usage Rules</a>"
+          using assets from Age of Empires II, and it is not endorsed by or affiliated with Microsoft.
+        </div>
+      </div>
+    </v-footer>
   </v-app>
 </template>
 
@@ -46,5 +64,10 @@ html {
       color: #42b983;
     }
   }
+}
+
+.my-footer {
+  text-align: center;
+  width: 100%;
 }
 </style>
