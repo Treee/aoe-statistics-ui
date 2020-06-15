1e180a96252d47b55708d410485cd2d2182df8b7
Treee <jonathon.lindemann@gmail.com>
2020-06-15 16:40:28 -0600

    adding vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,45 @@
 <template>
   <v-app>
-    <v-tabs fixed-tabs>
-      <v-tab to="/players">Players</v-tab>
-      <v-tab to="/teams">Teams</v-tab>
-      <v-tab to="/tournaments">Tournaments</v-tab>
-    </v-tabs>
-    <router-view></router-view>
+    <v-app-bar app>
+      <v-tabs fixed-tabs>
+        <v-tab to="/players">Players</v-tab>
+        <v-tab to="/teams">Teams</v-tab>
+      </v-tabs>
+    </v-app-bar>
+    <main>
+      <v-container fluid>
+        <router-view></router-view>
+      </v-container>
+    </main>
   </v-app>
 </template>
 
 <script>
 export default {
-  name: "App",
-
-  data: () => ({
-    //
-  })
+  name: "App"
 };
 </script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  overflow: hidden;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,18 +1,26 @@
 import Vue from 'vue';
 import vuetify from './plugins/vuetify';
+import VueRouter from 'vue-router';
 
 Vue.config.productionTip = false
 
-// import PlayerStatisticsTable from "./components/PlayerStatisticsTable";
+import App from "./App.vue";
+import PlayerStatisticsTable from "./components/PlayerStatisticsTable";
 
-// const routes = [
-//   { path: '/players', component: PlayerStatisticsTable },
-//   { path: '/teams', component: PlayerStatisticsTable },
-//   { path: '/tournaments', component: PlayerStatisticsTable }
-// ];
-// const router = new VueRouter({ routes });
+Vue.use(vuetify);
+Vue.use(VueRouter);
+
+const routes = [
+  { path: '/', component: PlayerStatisticsTable },
+  { path: '/players', component: PlayerStatisticsTable },
+  { path: '/teams', component: PlayerStatisticsTable },
+  { path: '/tournaments', component: PlayerStatisticsTable }
+];
+const router = new VueRouter({ routes, mode: 'hash' });
 
 new Vue({
+  el: '#app',
+  router,
   vuetify,
-  // router: router
-}).$mount('#app')
+  render: h => h(App)
+});
